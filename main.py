50ed03889961becbdd6ba470fb994265bbf8e412
Martin Maslyankov <m.maslyankov@me.com>
2020-08-24 18:33:28 +0300

    Functionality for photos capture is now working, connect to device works

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ def gui():
     # All the stuff inside your window.
     layout = [[sg.Image(r'.\images\automated-video-testing-header.png')],
               [
-                  sg.Listbox(values=list_devices(), size=(50, 3)),
-                  sg.Radio('Photos', "MODE", default=True), sg.Radio('Videos', "MODE"), sg.Radio('Both', "MODE"),
-                  sg.Spin([i for i in range(5, 60)], initial_value=10), sg.Text('Video Duration (secs)')
+                  sg.Listbox(values=list_devices(), size=(50, 3), key='device'),
+                  sg.Radio('Photos', "MODE", default=True, key='mode_photos'), sg.Radio('Videos', "MODE", key='mode_videos'), sg.Radio('Both', "MODE", key='mode_both'),
+                  sg.Spin([i for i in range(5, 60)], initial_value=10, key='duration_spinner'), sg.Text('Video Duration (secs)')
               ],
               [
-                  sg.Checkbox('Capture Logs', default=True, size=(13, 1)),
-                  sg.Text('Logs Filter'), sg.InputText(size=(77, 1))
+                  sg.Checkbox('Capture Logs', default=True, size=(13, 1), key='logs_bool'),
+                  sg.Text('Logs Filter'), sg.InputText(size=(77, 1), key='logs_filter')
               ],
-              [sg.Text('Save Location ', size=(15, 1)), sg.InputText(size=(80, 1)), sg.FolderBrowse()],
-              [sg.Button('Cancel', size=(10, 2)), sg.Button('Capture Case', size=(20, 2))],
+              [sg.Text('Save Location ', size=(15, 1)), sg.InputText(size=(80, 1), key='save_location'), sg.FolderBrowse()],
+              [sg.Button('Cancel', size=(10, 2)), sg.Button('Connect', size=(20, 2)), sg.Button('Capture Case', size=(20, 2))],
               [sg.Text('_' * 107)],
               [sg.Text('Application Logs', size=(70, 1))],
               [sg.Output(size=(105, 15))]
@@ -29,9 +29,36 @@ def gui():
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
         event, values = window.read()
+
+        if values['mode_photos']:
+            window.FindElement('duration_spinner').Update(disabled=True)
+
         if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
             break
         print('Data: ', values)
+        print("Event: ", event)
+
+        if event == "Connect":
+            # Assign device to object
+            if values['device']:
+                device = Device(values['device'][0])
+                device.open_snap_cam()
+            else:
+                print("First select a device!")
+
+        if event == "Capture Case":
+            # Photos Mode
+            if values['mode_photos']:
+                device.take_photo()
+
+            # Videos Mode
+            if values['mode_videos']:
+                shoot_video(device, values['duration_spinner'], values['logs_bool'], values['logs_filter'], "logfile.txt")
+
+            # Dual Mode
+            if values['mode_both']:
+                device.take_photo()
+                shoot_video(device, values['duration_spinner'], values['logs_bool'], values['logs_filter'], "logfile.txt")
 
     window.close()
 
