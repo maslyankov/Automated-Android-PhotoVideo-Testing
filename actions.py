cbb096e64edc88357bf288b460ce05304ca0c9ab
Martin Maslyankov <m.maslyankov@me.com>
2020-08-24 15:42:39 +0300

    Started work on GUI

--- actions.py
+++ actions.py
@@ -1,16 +1,26 @@
 from device import Device
+from get_logs import DeviceLogs
 import time
 
 
-def shoot_video(obj, duration=10):
+def shoot_video(obj, duration=10, with_logs=False, logs_filter="", logs_file=""):
     print("Starting Video")
     obj.start_video()
+
+    if with_logs:
+        # Logs
+        dlogs = DeviceLogs()
+        dlogs.start_logs(logs_filter, logs_file)
 
     print("Waiting {} secs".format(duration))
     time.sleep(duration)
 
     print("Ending video")
     obj.stop_video()
+
+    if with_logs:
+        # Logs
+        dlogs.stop_logs()
 
 def pull_camera_files(obj, folder, clear_after_pull):
     files_list = obj.get_camera_files_list()
--- main.py
+++ main.py
@@ -1,28 +1,48 @@
 from actions import *
-from get_logs import DeviceLogs
 from device import Device
+import PySimpleGUI as sg
 import time
 
 
 def main():
     device = Device()
-    dlogs = DeviceLogs()
-
-
 
     # shoot_video(device, 5)
 
     device.open_snap_cam()
-    device.take_photo()
-    #dlogs.start_logs("Gain:ExpTime", "logfile.txt")
-    #shoot_video(device, 10)
-    #dlogs.stop_logs()
+
+    #device.take_photo()
+
+    #shoot_video(device, 10, True, "Gain:ExpTime", "logfile.txt")
 
     #device.push_file("tuning/com.qti.tuned.snap_imx476.bin", "vendor/lib/camera")
-    #device.pull_file("sdcard/DCIM/Camera/IMG_20200804_152412.jpg", "./tests1/IMG_20200804_152412.jpg")
 
-    time.sleep(1)
-    pull_camera_files(device, "tests2", True)
+    #time.sleep(1)
+    #pull_camera_files(device, "tests2", True)
+
+
+    sg.theme('DarkBlue3')  # Add a touch of color
+    # All the stuff inside your window.
+    layout = [[sg.Text('Automated Photo/Video Testing', size=(50,3))],
+              [sg.Listbox(values=['Device 1', 'Device 2', 'Device 3'], size=(40, 6))],
+              [sg.Radio('Photos', "MODE", default=True), sg.Radio('Videos', "MODE"), sg.Radio('Both', "MODE")],
+              [sg.Checkbox('Capture Logs', default=True)],
+              [sg.Text('Logs Filter'), sg.InputText()],
+              [sg.Button('Cancel', size=(10, 2)), sg.Button('Capture Case', size=(20, 2))],
+              [sg.Text('Application Logs', size=(50, 1))],
+              [sg.Multiline('Log', size=(80, 15))]
+              ]
+
+    # Create the Window
+    window = sg.Window('Window Title', layout)
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == 'Cancel':  # if user closes window or clicks cancel
+            break
+        print('Device: ', values[0], "Radio Buttons: " ,values[1], values[2], values[3])
+
+    window.close()
 
 
 if __name__ == "__main__":
