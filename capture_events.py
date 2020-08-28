476472f7687f3051d2cfd7dbe0e36b633697f6ce
Martin Maslyankov <m.maslyankov@me.com>
2020-08-28 15:18:15 +0300

    Added more error handling, started work on adding lights, more stuff I don't remember

--- capture_events.py
+++ capture_events.py
@@ -1,67 +1,50 @@
 ## Original file - https://github.com/tzutalin/adb-event-record/blob/master/adbrecord.py
-
+import os
 import subprocess
 from subprocess import PIPE
 import re
 import time
 
-EVENT_LINE_RE = re.compile(r"(\S+): (\S+) (\S+) (\S+)$")
-STORE_LINE_RE = re.compile(r"(\S+) (\S+) (\S+) (\S+) (\S+)$")
+ELEVATED = False
 
-def dlog(msg):
-    print(str(msg))
 
-class CaptureEvents():
+def runCommand(command, forcesudo=False):
+    if 'sudo' in command.lower():
+        command = command.replace('sudo ', '')
+    if ELEVATED == True or forcesudo == True:
+        os.system('sudo {}'.format(command))
+    else:
+        os.system('{}'.format(command))
 
-    def record(self, fpath, eventNum=None):
-        print('Start recording')
-        record_command = self.adb_shell_command + [b'getevent']
-        adb = subprocess.Popen(record_command,
-                               stdin=PIPE, stdout=PIPE,
-                               stderr=PIPE)
 
-        outputFile = open(fpath, 'w')
-        while adb.poll() is None:
-            try:
-                millis = int(round(time.time() * 1000))
-                line = adb.stdout.readline().decode('utf-8', 'replace').strip()
-                match = EVENT_LINE_RE.match(line.strip())
-                if match is not None:
-                    dev, etype, ecode, data = match.groups()
-                    ## Filter event
-                    if eventNum is not None and '/dev/input/event%s' % (eventNum) != dev:
-                        continue
-                    ## Write to the file
-                    etype, ecode, data = int(etype, 16), int(ecode, 16), int(data, 16)
-                    rline = "%s %s %s %s %s\n" % (millis, dev, etype, ecode, data)
-                    dlog(rline)
-                    outputFile.write(rline)
-            except KeyboardInterrupt:
-                break
-            if len(line) == 0:
-                break
-        outputFile.close()
-        print('End recording')
+def getResponse(command):
+    os.system('{} > tmp'.format(command))
+    return open('tmp', 'r').read()
 
-    def play(self, fpath, repeat=False):
-        print('Start playing')
-        while True:
-            lastTs = None
-            with open(fpath) as fp:
-                for line in fp:
-                    match = STORE_LINE_RE.match(line.strip())
-                    ts, dev, etype, ecode, data = match.groups()
-                    ts = float(ts)
-                    if lastTs and (ts - lastTs) > 0:
-                        delta_second = (ts - lastTs) / 1000
-                        time.sleep(delta_second)
 
-                    lastTs = ts
-                    cmds = self.adb_shell_command + [b'sendevent', dev, etype, ecode, data]
-                    dlog(cmds)
-                    if subprocess.call(cmds) != 0:
-                        raise OSError('sendevent failed')
+def grabScreenResolution(udid):
+    adb = subprocess.Popen(('adb', '-s', udid, 'shell', 'dumpsys', 'window'), stdout=subprocess.PIPE) #  | grep "mUnrestricted"'
+    output = subprocess.Popen(['grep', 'mUnrestricted'],
+                                     stdin=adb.stdout,
+                                     stdout=subprocess.PIPE)
+    out = re.findall("\d+", str(output.stdout.read()))
+    return [out[2],out[3]]
 
-            if repeat == False:
-                break
-        print('End playing')
+
+def GrabUiAutomator(udid):
+    saved_file = '.\\XML\\' + str(udid) + '.xml'
+    command = "adb -s {} shell uiautomator dump && adb -s {} pull /sdcard/window_dump.xml {}".format(
+        udid, udid, saved_file)
+    print(command)
+    runCommand(command)
+    return saved_file
+
+
+def main():
+    # print(str(GrabUiAutomator("cf108b7")))
+    #print(grabScreenResolution("cf108b7"))
+    print("Hi")
+
+
+if __name__ == "__main__":
+    main()
