9762b541df9f9999d8ba76a3addcdfce9f33b21a
Martin Maslyankov <m.maslyankov@me.com>
2020-09-29 17:41:14 +0300

    Visual improvements to auto cases gui

--- src/gui/gui_automated_cases.py
+++ src/gui/gui_automated_cases.py
@@ -31,14 +31,14 @@ def gui_automated_cases(attached_devices, devices_obj, selected_lights_model, se
             sg.Text(text=devices_obj[attached_devices[0]].friendly_name,
                     key='device-friendly',
                     font="Any 18",
-                    auto_size_text=True)
+                    size=(18, 1))
         ],
     ]
 
     case_frame_layout = [[
         sg.Radio('Photos', "MODE", default=True, key='mode_photos', enable_events=True),
         sg.Radio('Videos', "MODE", key='mode_videos', enable_events=True),
-        sg.Radio('Both', "MODE", key='mode_both', enable_events=True),
+        sg.Radio('Both', "MODE", key='mode_both', enable_events=True, size=(8, 1)),
         sg.Spin([i for i in range(5, 60)], initial_value=10, key='duration_spinner', disabled=True),
         sg.Text('Video Duration (secs)')
     ],
@@ -51,7 +51,7 @@ def gui_automated_cases(attached_devices, devices_obj, selected_lights_model, se
         ],
         [
             sg.Text('Save Location:', size=(11, 1)),
-            sg.InputText(size=(35, 1), key='save_location', disabled=True, enable_events=True),
+            sg.InputText(size=(36, 1), key='save_location', disabled=True, enable_events=True),
             sg.FolderBrowse(key='save_location_browse_btn')
         ],
     ]
@@ -69,9 +69,9 @@ def gui_automated_cases(attached_devices, devices_obj, selected_lights_model, se
         [sg.Frame('After Case', post_case_frame_layout, font='Any 12', title_color='white')],
         [sg.Frame('Lights', lights_frame_layout, font='Any 12', title_color='white')],
 
-        [place(sg.Button('Stop', key='stop_case_btn', size=(13, 2), visible=False)), sg.Button('Do Cases', key='capture_case_btn', size=(40, 2))],
+        [sg.Button('Do Cases', key='capture_case_btn', size=(54, 2))],
 
-        [sg.Multiline(key='-OUT-', size=(61, 10), autoscroll=True)],
+        [sg.Multiline(key='-OUT-', size=(59, 10), autoscroll=True)],
         [sg.ProgressBar(max_value=100, orientation='h', size=(35, 5), key='progressbar', visible=False)],
         [
             place(sg.Text('0', key='progress_value', size=(3, 1), visible=False)),
@@ -81,6 +81,7 @@ def gui_automated_cases(attached_devices, devices_obj, selected_lights_model, se
 
     window = sg.Window('Automated Photo/Video Testing', layout,
                        icon=os.path.join(constants.ROOT_DIR, 'images', 'automated-video-testing-header-icon.ico'))
+    automation_is_running = False
 
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
@@ -119,19 +120,17 @@ def gui_automated_cases(attached_devices, devices_obj, selected_lights_model, se
                                   window, '-OUT-',
                                   specific_device=None if values['use_all_devices_bool'] else values['selected_device'])
             cases.execute('-AUTO-CASES-THREAD-')
-            # devices_obj[attached_devices[0]].pull_and_rename(os.path.normpath(values['save_location']), 'gosho')
-            # devices_obj[attached_devices[0]].clear_folder('sdcard/DCIM/Camera/')
-
-            window['progressbar'].Update(visible=True, current_count=0)
-            window['progress_value'].Update(visible=True)
-            window['progress_value_symbol'].Update(visible=True)
 
         if event == '-AUTO-CASES-THREAD-':
             window['progressbar'].UpdateBar(values["-AUTO-CASES-THREAD-"])
             window['progress_value'].Update(str(values["-AUTO-CASES-THREAD-"]))
 
             if values["-AUTO-CASES-THREAD-"] > 0:
-                window['stop_case_btn'].Update(visible=True)
+                window['capture_case_btn'].Update(button_text='Stop')
+
+                window['progressbar'].Update(visible=True, current_count=0)
+                window['progress_value'].Update(visible=True)
+                window['progress_value_symbol'].Update(visible=True)
 
             if values["-AUTO-CASES-THREAD-"] == 100:
                 sg.Popup('Cases done!')
