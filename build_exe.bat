3d3244c98644a5972963d7f29d52b2a0274bab7f
Martin Maslyankov <m.maslyankov@me.com>
2020-12-21 21:37:32 +0200

    Updated app version

--- build_exe.bat
+++ build_exe.bat
@@ -1,5 +1,5 @@
 set exe_name="MM-Automations-App"
-set app_version="0-06-Beta"
+set app_version="0-07-Beta"
 
 python -m PyInstaller --add-data "vendor;vendor" --add-data "data;data" --add-data "images;images" --name "%exe_name%_%app_version%" --noupx --noconsole --onefile -i ./images/automated-video-testing-header-icon.ico ./automated_cases.py
 python -m PyInstaller --add-data "vendor;vendor" --add-data "data;data" --add-data "images;images" --name "%exe_name%_%app_version%_console" --noupx --onefile -i ./images/automated-video-testing-header-icon.ico ./automated_cases.py
