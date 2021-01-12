504ecb0701899444bd9cc7161b6d6621856a9367
Martin Maslyankov <mmaslyankov@fb.com>
2021-01-12 16:19:21 +0200

    Created ObjectiveReports.py for objective reports restructuring

---
+++ src/code/reports/ObjectiveReports.py
@@ -0,0 +1,8 @@
+from src import constants
+from src.logs import logger
+from src.gui.utils_gui import send_progress_to_gui, send_error_to_gui
+
+
+class ObjectiveReports:
+    def __init__(self):
+        pass
