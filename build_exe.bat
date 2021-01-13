adf9bd5cbb625849713b8445bd6bbdb2e0dde503
Martin Maslyankov <m.maslyankov@me.com>
2021-01-13 20:20:44 +0200

    Up app version to 1.10

--- build_exe.bat
+++ build_exe.bat
@@ -1,5 +1,5 @@
 set exe_name="MM-Automations-App"
-set app_version="0-09-Beta"
+set app_version="0-10-Beta"
 
 python -m PyInstaller --add-data "vendor;vendor" --add-data "data;data" --add-data "images;images" --name "%exe_name%_%app_version%" --noupx --noconsole --onefile -i ./images/automated-video-testing-header-icon.ico ./automated_cases.py
 python -m PyInstaller --add-data "vendor;vendor" --add-data "data;data" --add-data "images;images" --name "%exe_name%_%app_version%_unpacked" --noconsole --noupx -i ./images/automated-video-testing-header-icon.ico ./automated_cases.py
